9c80dcbe089a51a7d7d312b22a53b926eee85ebd
Dele <oladele@gmail.com>
2014-05-14 09:34:16 -0400

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 X1App::Application.routes.draw do
+  get "static_pages/landing"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
