6add1538423a44e2273a1c149fff1cb01c0b3624
Dele <oladele@gmail.com>
2014-05-15 09:54:42 -0400

    initial specs for coming-soon page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,4 +14,17 @@ describe "StaticPages" do
       expect(page).to have_content('Xtronger')
     end
   end
+
+  describe "Coming-soon page" do
+
+    it "should have the right title" do
+      visit '/static_pages/coming_soon'
+      expect(page).to have_title("Xtronger")
+    end    
+
+    it "should have the content 'Coming soon'" do
+      visit root_path
+      expect(page).to have_content('Coming soon')
+    end
+  end
 end
